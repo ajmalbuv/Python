b365eab1f22d73ae32e69f89626d025660660768
Ajmal Basheer <77728850+ajmalbuv@users.noreply.github.com>
2023-06-26 22:31:06 +0530

    added 4 programs

---
+++ parta3.py
@@ -0,0 +1,12 @@
+#Sum of N natural numbers
+num=int(input("Enter the number: "))
+
+if num < 0:
+   print("Enter a positive number")
+else:
+   sum = 0
+   # use while loop to iterate until zero
+   while(num > 0):
+       sum += num
+       num -= 1
+   print("The sum is", sum)
